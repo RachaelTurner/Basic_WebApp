1828eb6cdd3f3a133235be6754f42360a6888711
Rachael Turner <rachaelvturner@btinternet.com>
2017-04-06 15:32:45 +0100

    Created image selecting app

--- webapp/src/main/webapp/controller/shell.controller.js
+++ webapp/src/main/webapp/controller/shell.controller.js
@@ -6,7 +6,23 @@ sap.ui.define([
 
     return Controller.extend("com.rach.controller.shell",{
         onInit: function () {
+            this.viewModel = new JSONModel();
+            this.getView().setModel(this.viewModel);
+            this.viewData = {
+                selectedImage:"https://i.ytimg.com/vi/zecueq-mo4M/maxresdefault.jpg"
+            };
+            this.viewModel.setData(this.viewData);
+        },
 
+        onChoosePress: function () {
+            var dialogView = sap.ui.xmlview("com.rach.view.ChooseDialog");
+            this.getView().addDependent(dialogView);
+            var dialogController = dialogView.getController();
+            dialogController.showDialog(this.getView().getController());
+        },
+
+        setSelectedImage: function (selectedImageURL) {
+            this.viewModel.setProperty("/selectedImage", selectedImageURL);
         }
     });
 });
